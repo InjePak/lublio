4d9e2a43b290baeedd4dde02a7dd96196c0d8310
Inje <inje.ian.pak@gmail.com>
2026-09-16 12:41:45 +0900

    Change the whole CSS

---
+++ src/data/patterns/french.ts
@@ -0,0 +1,23 @@
+import chapter1 from "../../assets/json/patterns/french/chapter1.json";
+import chapter2 from "../../assets/json/patterns/french/chapter2.json";
+import chapter3 from "../../assets/json/patterns/french/chapter3.json";
+import chapter4 from "../../assets/json/patterns/french/chapter4.json";
+import chapter5 from "../../assets/json/patterns/french/chapter5.json";
+import chapter6 from "../../assets/json/patterns/french/chapter6.json";
+import chapter7 from "../../assets/json/patterns/french/chapter7.json";
+import chapter8 from "../../assets/json/patterns/french/chapter8.json";
+import chapter9 from "../../assets/json/patterns/french/chapter9.json";
+import chapter10 from "../../assets/json/patterns/french/chapter10.json";
+
+export const frenchChapters: Record<number, typeof chapter1> = {
+  1: chapter1,
+  2: chapter2,
+  3: chapter3,
+  4: chapter4,
+  5: chapter5,
+  6: chapter6,
+  7: chapter7,
+  8: chapter8,
+  9: chapter9,
+  10: chapter10,
+};
